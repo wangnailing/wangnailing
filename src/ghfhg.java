26e55e14e9d30539eba5448a8c810d43baf839bf
wangnailing <a1440381979@163.com>
2021-11-04 14:16:24 +0800

    fff

--- src/ghfhg.java
+++ src/ghfhg.java
@@ -3,5 +3,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class ghfhg {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println();
     }
 }
